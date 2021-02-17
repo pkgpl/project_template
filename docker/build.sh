f93f31bf33a37060c21fbf7f20583d042f6e3d93
Wansoo Ha <wansooha@gmail.com>
2021-02-17 03:10:45 -0500

    build option with/without cache

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/sh 
+#!/bin/sh 
 
 source ./Env.sh
+
+if [ $# -gt 0 ] && [ $1 == "-h" ]; then
+    echo "Run Container: $PROJECT_NAME"
+    echo "$0 [argument]"
+    echo "  no argument: docker build without cache"
+    echo "  -c:          docker build with cache"
+    exit 0
+fi
 
 if [ $DEVICE == "gpu" ]; then
     DOCKERFROM=$GPU_FROM
@@ -8,13 +16,29 @@ else
     DOCKERFROM=$CPU_FROM
 fi
 
+if [ $# == 0 ]; then
+    CACHE="--no-cache"
+else
+    case $1 in
+        -c)
+            CACHE=""
+            ;;
+        *)
+            echo ""
+            echo "Error: Wrong argument"
+            exit 1
+            ;;
+    esac
+fi
+
 echo "Building new docker image for '$USER_NAME' (uid=$USER_ID, gid=$GROUP_ID), device=$DEVICE"
 echo "  image name: $IMAGE_NAME:$IMAGE_TAG"
 echo "  project directory: $PROJECT_DIR"
 
+
 docker build \
   --rm \
-  --no-cache \
+  $CACHE \
   --build-arg dockerfrom=$DOCKERFROM \
   --build-arg username=$USER_NAME \
   --build-arg uid=$USER_ID \
@@ -23,3 +47,4 @@ docker build \
   --file Dockerfile \
   --tag $IMAGE_NAME:$IMAGE_TAG \
   ../
+
